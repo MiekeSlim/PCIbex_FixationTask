5dbe25e4fb574f867d7a818d1943d614fafd38cc
MiekeSlim <74954820+MiekeSlim@users.noreply.github.com>
2020-12-04 15:36:22 +0100

    Update PennElement_eyetracker.js

--- js_includes/PennElement_eyetracker.js
+++ js_includes/PennElement_eyetracker.js
@@ -130,7 +130,7 @@ window.PennController._AddElementType("EyeTracker", function(PennEngine) {
          // Launches calculation per se
         $(this).attr('disabled', true);
         calibrationDiv.find('button').remove();
-        calibrationDiv.append($("<p>div</p>").html("<p>+</p>").css({position: 'absolute', top: "calc(50vh - 1.5vw)", left: "48.5vw", 'text-align': 'center', color: "red", "font-size": "10vw"}));    
+        calibrationDiv.append($("<p>+</p>").css({position: 'absolute', top: "calc(50vh - 5vw)", left: "45vw", color: "red", "font-size": "10vw"}));    
                 storePoints = true;
                 setTimeout(()=>{
                     console.log("Past 50", past50Array);
